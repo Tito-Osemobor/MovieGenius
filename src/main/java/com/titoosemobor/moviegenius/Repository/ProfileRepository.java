f0787ef4c725c3371d469bc76867db1b0b6a9c1d
Tito-Osemobor <titoosemobor@gmail.com>
2023-07-22 19:13:46 -0400

    Add @Repository annotation

--- src/main/java/com/titoosemobor/moviegenius/Repository/ProfileRepository.java
+++ src/main/java/com/titoosemobor/moviegenius/Repository/ProfileRepository.java
@@ -2,6 +2,8 @@ package com.titoosemobor.moviegenius.Repository;
 
 import com.titoosemobor.moviegenius.Entity.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
 }
--- src/main/java/com/titoosemobor/moviegenius/Repository/UserRepository.java
+++ src/main/java/com/titoosemobor/moviegenius/Repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.titoosemobor.moviegenius.Repository;
 
 import com.titoosemobor.moviegenius.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
