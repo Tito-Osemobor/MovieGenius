41aabc166959a76d78d5aa8fd01feb3ac0b3210c
Tito-Osemobor <titoosemobor@gmail.com>
2023-07-24 20:56:20 -0400

    Replace User with UserDTO

--- src/main/java/com/titoosemobor/moviegenius/Controller/UserController.java
+++ src/main/java/com/titoosemobor/moviegenius/Controller/UserController.java
@@ -1,14 +1,16 @@
 package com.titoosemobor.moviegenius.Controller;
 
-import com.titoosemobor.moviegenius.Entity.User;
+import com.titoosemobor.moviegenius.DataTransferObject.UserDTO;
 import com.titoosemobor.moviegenius.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -16,7 +18,12 @@ public class UserController {
   private UserService userService;
 
   @GetMapping("/users")
-  public ResponseEntity<List<User>> getUsers () {
+  public ResponseEntity<List<UserDTO>> getUsers () {
     return new ResponseEntity<>(userService.allUsers(), HttpStatus.OK);
+  }
+
+  @GetMapping("/users/{id}")
+  public ResponseEntity<Optional<UserDTO>> getUserById(@PathVariable Long id) {
+    return new ResponseEntity<>(userService.userById(id), HttpStatus.OK);
   }
 }
--- src/main/java/com/titoosemobor/moviegenius/Service/UserService.java
+++ src/main/java/com/titoosemobor/moviegenius/Service/UserService.java
@@ -1,18 +1,32 @@
 package com.titoosemobor.moviegenius.Service;
 
-import com.titoosemobor.moviegenius.Entity.User;
+import com.titoosemobor.moviegenius.DTOMapper.UserDTOMapper;
+import com.titoosemobor.moviegenius.DataTransferObject.UserDTO;
 import com.titoosemobor.moviegenius.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
   @Autowired
   private UserRepository userRepository;
 
-  public List<User> allUsers() {
-    return userRepository.findAll();
+  @Autowired
+  private UserDTOMapper userDTOMapper;
+
+  public List<UserDTO> allUsers() {
+    return userRepository.findAll()
+      .stream()
+      .map(userDTOMapper)
+      .collect(Collectors.toList());
+  }
+
+  public Optional<UserDTO> userById(Long id) {
+    return userRepository.findById(id)
+      .map(userDTOMapper);
   }
 }
