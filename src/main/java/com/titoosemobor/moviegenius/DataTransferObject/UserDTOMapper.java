a4943b1c6fa0455e95ae7ecf7563171292ab0ca7
Tito-Osemobor <titoosemobor@gmail.com>
2023-07-24 22:12:37 -0400

    Rename request DTO for User

---
+++ src/main/java/com/titoosemobor/moviegenius/DataTransferObject/UserDTOMapper.java
@@ -0,0 +1,19 @@
+package com.titoosemobor.moviegenius.DataTransferObject;
+
+import com.titoosemobor.moviegenius.Entity.User;
+import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
+
+@Service
+public class UserDTOMapper implements Function<User, UserDTOResponse> {
+  @Override
+  public UserDTOResponse apply(User user) {
+    return new UserDTOResponse(
+      user.getId(),
+      user.getEmail(),
+      user.getCreated_at(),
+      user.getProfiles()
+    );
+  }
+}
