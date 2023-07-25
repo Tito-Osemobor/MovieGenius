331d65773e2ff14a9a22e6f008250f4694bc8c74
Tito-Osemobor <titoosemobor@gmail.com>
2023-07-24 23:46:27 -0400

    Create endpoint for login and sign-up

--- src/main/java/com/titoosemobor/moviegenius/Controller/UserController.java
+++ src/main/java/com/titoosemobor/moviegenius/Controller/UserController.java
@@ -1,13 +1,12 @@
 package com.titoosemobor.moviegenius.Controller;
 
+import com.titoosemobor.moviegenius.DTO.UserDTORequest;
 import com.titoosemobor.moviegenius.DTO.UserDTOResponse;
 import com.titoosemobor.moviegenius.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -25,5 +24,15 @@ public class UserController {
   @GetMapping("/users/{id}")
   public ResponseEntity<Optional<UserDTOResponse>> getUserById(@PathVariable Long id) {
     return new ResponseEntity<>(userService.userById(id), HttpStatus.OK);
+  }
+
+  @PostMapping("/login")
+  public ResponseEntity<Optional<UserDTOResponse>> getUserByEmailAndPassword(@RequestBody UserDTORequest userDTORequest) {
+    return new ResponseEntity<>(userService.userByEmailAndPassword(userDTORequest), HttpStatus.OK);
+  }
+
+  @PostMapping("/sign-up")
+  public ResponseEntity<String> createNewUser(@RequestBody UserDTORequest userDTORequest) {
+    return null;
   }
 }
