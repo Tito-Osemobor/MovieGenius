5b32d3a52e3964f781b13e7e511602d74b332027
Tito-Osemobor <titoosemobor@gmail.com>
2023-08-02 23:50:35 -0400

    Add autowired annotation

--- src/main/java/com/titoosemobor/moviegenius/Auth/AuthenticationController.java
+++ src/main/java/com/titoosemobor/moviegenius/Auth/AuthenticationController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
-
+  @Autowired
   private final AuthenticationService service;
   @PostMapping("/register")
   public ResponseEntity<AuthenticationResponse> register (
