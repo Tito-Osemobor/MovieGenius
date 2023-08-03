08b6dd58880d86aa18b48da3e63e7b445bcf6bfe
Tito-Osemobor <titoosemobor@gmail.com>
2023-08-02 23:57:03 -0400

    Change endpoints and add exception handling

--- src/main/java/com/titoosemobor/moviegenius/Controller/UserController.java
+++ src/main/java/com/titoosemobor/moviegenius/Controller/UserController.java
@@ -1,7 +1,9 @@
 package com.titoosemobor.moviegenius.Controller;
 
+import com.titoosemobor.moviegenius.DTO.PasswordUpdateDTO;
 import com.titoosemobor.moviegenius.DTO.UserDTORequest;
 import com.titoosemobor.moviegenius.DTO.UserDTOResponse;
+import com.titoosemobor.moviegenius.Exception.UserException;
 import com.titoosemobor.moviegenius.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,17 +14,17 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/moviegenius")
+@RequestMapping("/moviegenius/users")
 public class UserController {
   @Autowired
   private UserService userService;
 
-  @GetMapping("/users")
-  public ResponseEntity<List<UserDTOResponse>> getUsers () {
+  @GetMapping
+  public ResponseEntity<List<UserDTOResponse>> getUsers() {
     return new ResponseEntity<>(userService.allUsers(), HttpStatus.OK);
   }
 
-  @GetMapping("/users/{id}")
+  @GetMapping("/{id}")
   public ResponseEntity<Optional<UserDTOResponse>> getUserById(@PathVariable Long id) {
     return new ResponseEntity<>(userService.userById(id), HttpStatus.OK);
   }
@@ -32,8 +34,23 @@ public class UserController {
     return new ResponseEntity<>(userService.userByEmailAndPassword(userDTORequest), HttpStatus.OK);
   }
 
-  @PostMapping("/signup")
-  public ResponseEntity<String> createNewUser(@RequestBody UserDTORequest userDTORequest) {
-    return null;
+  @PutMapping("/update")
+  public ResponseEntity<?> updateUserPassword(@RequestBody PasswordUpdateDTO passwordUpdateDTO) {
+    try {
+      Optional<UserDTOResponse> userDTOResponseOptional = userService.updateUserPassword(passwordUpdateDTO);
+      if (userDTOResponseOptional.isEmpty()) {
+        return ResponseEntity.badRequest().body("Invalid email or password");
+      }
+      return new ResponseEntity<>(userDTOResponseOptional, HttpStatus.OK);
+    } catch (UserException.InvalidInputException ex) {
+      return ResponseEntity.badRequest().body(ex.getMessage());
+    } catch (UserException.PasswordMismatchException ex) {
+      return ResponseEntity.badRequest().body(ex.getMessage());
+    } catch (UserException.UserNotFoundException ex) {
+      return ResponseEntity.notFound().build();
+    } catch (Exception ex) {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred during password update.");
+    }
+
   }
 }
