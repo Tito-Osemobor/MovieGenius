e63d7496386508ccbd7b434c958b3f9d189de1dc
Tito-Osemobor <titoosemobor@gmail.com>
2023-08-03 23:27:28 -0400

    modify UserDTORequest structure

--- src/main/java/com/titoosemobor/moviegenius/DTO/UserDTORequest.java
+++ src/main/java/com/titoosemobor/moviegenius/DTO/UserDTORequest.java
@@ -1,23 +1,22 @@
 package com.titoosemobor.moviegenius.DTO;
 
-import com.titoosemobor.moviegenius.Entity.Profile;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Setter
 public class UserDTORequest {
   private String email;
   private String password;
-  private List<Profile> profiles;
+
+  public UserDTORequest(String email) {
+    this.email = email;
+  }
 
   public UserDTORequest(String email, String password) {
     this.email = email;
     this.password = password;
-    this.profiles = new ArrayList<>();
   }
 }
 
