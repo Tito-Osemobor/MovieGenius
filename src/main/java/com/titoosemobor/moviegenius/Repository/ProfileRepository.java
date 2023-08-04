e21cd3dfc864ab6fb91096a24071f52a1cda78fa
Tito-Osemobor <titoosemobor@gmail.com>
2023-08-04 14:24:14 -0400

    Add custom repository queries

--- src/main/java/com/titoosemobor/moviegenius/Repository/ProfileRepository.java
+++ src/main/java/com/titoosemobor/moviegenius/Repository/ProfileRepository.java
@@ -1,9 +1,14 @@
 package com.titoosemobor.moviegenius.Repository;
 
 import com.titoosemobor.moviegenius.Entity.Profile;
+import com.titoosemobor.moviegenius.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
+  List<Profile> findProfilesByUser(User user);
+  Profile findProfileById(Long id);
 }
