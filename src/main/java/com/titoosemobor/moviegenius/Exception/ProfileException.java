3a3f449831966759b1b8369f298a8c46f34102b8
Tito-Osemobor <titoosemobor@gmail.com>
2023-08-06 00:44:53 -0400

    Create profile exceptions

---
+++ src/main/java/com/titoosemobor/moviegenius/Exception/ProfileException.java
@@ -0,0 +1,21 @@
+package com.titoosemobor.moviegenius.Exception;
+
+public class ProfileException extends RuntimeException {
+  public static class NameAlreadyInUse extends RuntimeException {
+    public NameAlreadyInUse(String message) {
+      super(message);
+    }
+  }
+
+  public static class MaxNumberOfProfiles extends RuntimeException {
+    public MaxNumberOfProfiles(String message) {
+      super(message);
+    }
+  }
+
+  public static class ProfileNotFound extends RuntimeException {
+    public ProfileNotFound(String message) {
+      super(message);
+    }
+  }
+}
