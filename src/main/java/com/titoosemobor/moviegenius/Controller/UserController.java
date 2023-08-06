40a214e39cb2c5a19fbf39d4e0cdbca735312495
Tito-Osemobor <titoosemobor@gmail.com>
2023-08-05 23:54:08 -0400

    Add Exception and Error handling

--- src/main/java/com/titoosemobor/moviegenius/Controller/UserController.java
+++ src/main/java/com/titoosemobor/moviegenius/Controller/UserController.java
@@ -1,13 +1,14 @@
 package com.titoosemobor.moviegenius.Controller;
 
 import com.titoosemobor.moviegenius.DTO.PasswordUpdateDTO;
-import com.titoosemobor.moviegenius.DTO.UserDTORequest;
-import com.titoosemobor.moviegenius.DTO.UserDTOResponse;
+import com.titoosemobor.moviegenius.DTO.UserDTOResponse ;
+import com.titoosemobor.moviegenius.Entity.User;
 import com.titoosemobor.moviegenius.Exception.UserException;
 import com.titoosemobor.moviegenius.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,32 +26,44 @@ public class UserController {
   }
 
   @GetMapping("/{id}")
-  public ResponseEntity<Optional<UserDTOResponse>> getUserById(@PathVariable Long id) {
-    return new ResponseEntity<>(userService.userById(id), HttpStatus.OK);
-  }
-
-  @PostMapping("/login")
-  public ResponseEntity<Optional<UserDTOResponse>> getUserByEmailAndPassword(@RequestBody UserDTORequest userDTORequest) {
-    return new ResponseEntity<>(userService.userByEmailAndPassword(userDTORequest), HttpStatus.OK);
+  public ResponseEntity<?> getUserById(@PathVariable Long id,
+                                       @AuthenticationPrincipal User authUser) {
+    try {
+      if (isOwnedByAuthenticatedUser(userService.userById(id).get(), authUser)) {
+        return new ResponseEntity<>(userService.userById(id), HttpStatus.OK);
+      }
+      return ResponseEntity.status(HttpStatus.FORBIDDEN)
+        .body("You are not authorized to access this profile.");
+    } catch (UserException.UserNotFoundException ex){
+      return ResponseEntity.badRequest().body(ex.getMessage());
+    }
   }
 
   @PutMapping("/update")
-  public ResponseEntity<?> updateUserPassword(@RequestBody PasswordUpdateDTO passwordUpdateDTO) {
+  public ResponseEntity<?> updateUserPassword(@RequestBody PasswordUpdateDTO passwordUpdateDTO,
+                                              @AuthenticationPrincipal User authUser) {
     try {
-      Optional<UserDTOResponse> userDTOResponseOptional = userService.updateUserPassword(passwordUpdateDTO);
-      if (userDTOResponseOptional.isEmpty()) {
-        return ResponseEntity.badRequest().body("Invalid email or password");
+      if (isOwnedByAuthenticatedUser(userService.userByEmail(passwordUpdateDTO.getEmail()).get(), authUser)) {
+        Optional<UserDTOResponse> userDTOResponseOptional = userService.updateUserPassword(passwordUpdateDTO);
+        if (userDTOResponseOptional.isEmpty()) {
+          return ResponseEntity.badRequest().body("Incorrect email and/or password");
+        }
+        return new ResponseEntity<>(userDTOResponseOptional, HttpStatus.OK);
       }
-      return new ResponseEntity<>(userDTOResponseOptional, HttpStatus.OK);
+      return ResponseEntity.status(HttpStatus.FORBIDDEN)
+        .body("You are not authorized to access this profile");
     } catch (UserException.InvalidInputException ex) {
       return ResponseEntity.badRequest().body(ex.getMessage());
     } catch (UserException.PasswordMismatchException ex) {
       return ResponseEntity.badRequest().body(ex.getMessage());
     } catch (UserException.UserNotFoundException ex) {
-      return ResponseEntity.notFound().build();
+      return ResponseEntity.badRequest().body(ex.getMessage());
     } catch (Exception ex) {
       return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred during password update.");
     }
+  }
 
+  public boolean isOwnedByAuthenticatedUser(UserDTOResponse authUser, User requestUser) {
+    return authUser.getEmail().equals(requestUser.getEmail());
   }
 }
--- src/main/java/com/titoosemobor/moviegenius/Service/UserService.java
+++ src/main/java/com/titoosemobor/moviegenius/Service/UserService.java
@@ -32,28 +32,38 @@ public class UserService {
   }
 
   public Optional<UserDTOResponse> userById(Long id) {
-    return userRepository.findById(id)
+    Optional<User> userOptional = userRepository.findUserById(id);
+    if(userOptional.isEmpty()) {
+      throw new UserException.UserNotFoundException("User Not Found");
+    }
+    return userOptional
       .map(userDTOMapper);
   }
 
-  public Optional<UserDTOResponse> userByEmailAndPassword(UserDTORequest userDTORequest) {
-    return userRepository.findUserByEmailAndPassword(userDTORequest.getEmail(), userDTORequest.getPassword())
+  public Optional<UserDTOResponse> userByEmail(String email) {
+    Optional<User> userOptional = userRepository.findUserByEmail(email);
+    if (userOptional.isEmpty()) {
+      throw new UserException.UserNotFoundException("User not found");
+    }
+    return userOptional
       .map(userDTOMapper);
   }
 
   public Optional<UserDTOResponse> updateUserPassword(PasswordUpdateDTO passwordUpdateDTO) {
-    if (passwordUpdateDTO.getCurrentPassword() == null ||
-      passwordUpdateDTO.getNewPassword() == null) {
+    if (passwordUpdateDTO.getCurrentPassword().isEmpty()||
+      passwordUpdateDTO.getNewPassword().isEmpty()) {
       throw new UserException.InvalidInputException("Enter current and new password");
     }
-
+    if (passwordUpdateDTO.getCurrentPassword().equals(passwordUpdateDTO.getNewPassword())) {
+      throw new UserException.PasswordMismatchException("Current and New password must not match");
+    }
     if(!passwordUpdateDTO.getNewPassword().equals(passwordUpdateDTO.getReNewPassword())) {
       throw new UserException.PasswordMismatchException("New password and re-entered password do not match");
     }
     UserDTORequest userDTORequest = new UserDTORequest(passwordUpdateDTO.getEmail(), passwordUpdateDTO.getCurrentPassword());
     Optional<User> userToUpdateOptional = userRepository.findUserByEmail(userDTORequest.getEmail());
     if (userToUpdateOptional.isEmpty()) {
-      throw new UserException.UserNotFoundException("Incorrect email or password");
+      throw new UserException.UserNotFoundException("Incorrect email and/or password");
     }
     if (passwordEncoder.matches(userDTORequest.getPassword(), userToUpdateOptional.get().getPassword())) {
       User userToUpdate = userToUpdateOptional.get();
@@ -63,7 +73,7 @@ public class UserService {
       return Optional.of(userDTOResponse);
     }
     else {
-      throw new UserException.InvalidInputException("Incorrect email or password");
+      throw new UserException.UserNotFoundException("Incorrect email and/or password");
     }
   }
 }
