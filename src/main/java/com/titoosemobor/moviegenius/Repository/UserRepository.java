103ad2bf021e2fee9dc3165d9f67602d3b3d518d
Tito-Osemobor <titoosemobor@gmail.com>
2023-08-06 00:50:49 -0400

    New repository query

--- src/main/java/com/titoosemobor/moviegenius/Repository/UserRepository.java
+++ src/main/java/com/titoosemobor/moviegenius/Repository/UserRepository.java
@@ -8,6 +8,6 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-  Optional<User> findUserByEmailAndPassword(String email, String password);
   Optional<User> findUserByEmail(String email);
+  Optional<User> findUserById(Long id);
 }
