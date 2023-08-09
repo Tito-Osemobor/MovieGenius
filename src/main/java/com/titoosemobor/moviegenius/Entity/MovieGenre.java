b565072f3950a795ccdee75b5a7c1cab2f68ac16
Tito-Osemobor <titoosemobor@gmail.com>
2023-08-08 23:19:35 -0400

    Create class for Many-to-Many Movie<->Genius relationship

---
+++ src/main/java/com/titoosemobor/moviegenius/Entity/MovieGenre.java
@@ -0,0 +1,27 @@
+package com.titoosemobor.moviegenius.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "movies_genres")
+public class MovieGenre {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "movie_id")
+  private Movie movie;
+
+  @ManyToOne
+  @JoinColumn(name = "genre_id")
+  private Genre genre;
+}
