8b58133e9cb3846cb3a0963753097603e4c64b2c
Tito-Osemobor <titoosemobor@gmail.com>
2023-08-08 23:19:56 -0400

    Create genre repository for database

---
+++ src/main/java/com/titoosemobor/moviegenius/Repository/GenreRepository.java
@@ -0,0 +1,12 @@
+package com.titoosemobor.moviegenius.Repository;
+
+import com.titoosemobor.moviegenius.Entity.Genre;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Set;
+
+@Repository
+public interface GenreRepository extends JpaRepository<Genre, Long> {
+  Set<Genre> findAllByIdIn(Set<Long> ids);
+}
