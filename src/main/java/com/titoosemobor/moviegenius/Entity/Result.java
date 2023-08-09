8548be42adea2a9f055142a5211616aa71acad4e
Tito-Osemobor <titoosemobor@gmail.com>
2023-08-08 23:18:11 -0400

    Create class to represent a video result

---
+++ src/main/java/com/titoosemobor/moviegenius/Entity/Result.java
@@ -0,0 +1,15 @@
+package com.titoosemobor.moviegenius.Entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Result {
+  private String name;
+  private String key;
+  private boolean official;
+}
