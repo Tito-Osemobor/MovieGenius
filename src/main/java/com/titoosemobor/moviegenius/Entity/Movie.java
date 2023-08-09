a0db7d4c27829ef969aae67840889127675a96c0
Tito-Osemobor <titoosemobor@gmail.com>
2023-08-08 23:18:46 -0400

    Add fields and database logic

--- src/main/java/com/titoosemobor/moviegenius/Entity/Movie.java
+++ src/main/java/com/titoosemobor/moviegenius/Entity/Movie.java
@@ -1,29 +1,63 @@
 package com.titoosemobor.moviegenius.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@Table(name = "movies")
 public class Movie {
   @Id
-  @GeneratedValue
-  private Long movie_id;
+  @Column(name = "movie_id")
+  private Long id;
 
-  public void setMovie_id(Long movieId) {
-    this.movie_id = movieId;
+  private String imdb_id;
+
+  private String title;
+
+  @ManyToMany(fetch = FetchType.LAZY)
+  @JoinTable(
+    name = "movies_genres",
+    joinColumns = @JoinColumn(name = "movie_id"),
+    inverseJoinColumns = @JoinColumn(name = "genre_id")
+  )
+  private Set<Genre> genres = new HashSet<>();
+
+  @JsonProperty("backdrop_path")
+  @Column(name = "backdrop_path")
+  private String backdropPath;
+
+  @JsonProperty("poster_path")
+  @Column(name = "poster_path")
+  private String posterPath;
+
+  private String overview;
+
+  @Transient
+  @JsonProperty("videos")
+  private Video video;
+
+  private String trailer;
+
+  public String getTrailer() {
+    Result officialResult = video.getResults().stream()
+      .filter(Result::isOfficial)
+      .findFirst()
+      .orElse(video.getResults().get(0));
+    return "https://www.youtube.com/watch?v=" + officialResult.getKey();
   }
 
-  public Long getMovie_id() {
-    return movie_id;
+  public void setTrailer() {
+    this.trailer = getTrailer();
   }
 }
