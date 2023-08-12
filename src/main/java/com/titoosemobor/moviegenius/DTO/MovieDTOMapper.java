2e53d95040690ee1248b3e12230b0cc74ad0b808
Tito-Osemobor <titoosemobor@gmail.com>
2023-08-12 12:43:51 -0400

    Create mapper for movie DTO

---
+++ src/main/java/com/titoosemobor/moviegenius/DTO/MovieDTOMapper.java
@@ -0,0 +1,27 @@
+package com.titoosemobor.moviegenius.DTO;
+
+import com.titoosemobor.moviegenius.Entity.Genre;
+import com.titoosemobor.moviegenius.Entity.Movie;
+import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Service
+public class MovieDTOMapper implements Function<Movie, MovieDTO> {
+
+  public static final MovieDTOMapper INSTANCE = new MovieDTOMapper();
+  @Override
+  public MovieDTO apply(Movie movie) {
+    return new MovieDTO(
+      movie.getTitle(),
+      movie.getGenres().stream()
+        .map(Genre::getName)
+        .collect(Collectors.toSet()),
+      "https://image.tmdb.org/t/p/w500" + movie.getBackdropPath(),
+      "https://image.tmdb.org/t/p/w500" + movie.getPosterPath(),
+      movie.getOverview(),
+      "https://www.youtube.com/watch?v=" + movie.getTrailer()
+    );
+  }
+}
