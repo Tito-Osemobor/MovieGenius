e49b1f0a2845d12b85c9451a073415ea28935e4c
Tito-Osemobor <titoosemobor@gmail.com>
2023-08-19 22:53:24 -0400

    Class to receive list of movies from api

---
+++ src/main/java/com/titoosemobor/moviegenius/Entity/Discover.java
@@ -0,0 +1,19 @@
+package com.titoosemobor.moviegenius.Entity;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.titoosemobor.moviegenius.DTO.MovieDTORequest;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Discover {
+  private long page;
+  @JsonProperty("results")
+  private Set<MovieDTORequest> results;
+}
