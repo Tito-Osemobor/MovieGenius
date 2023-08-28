01ae4ab433e0f0125f76b8f7bfbd37f7bca95b61
Tito-Osemobor <titoosemobor@gmail.com>
2023-08-28 10:42:58 -0400

    New repository class for fetching movies by genre(s)

---
+++ src/main/java/com/titoosemobor/moviegenius/Repository/MovieGenreRepository.java
@@ -0,0 +1,17 @@
+package com.titoosemobor.moviegenius.Repository;
+
+import com.titoosemobor.moviegenius.Entity.Movie;
+import com.titoosemobor.moviegenius.Entity.MovieGenre;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Set;
+
+@Repository
+public interface MovieGenreRepository extends JpaRepository<MovieGenre, Long> {
+  @Query("SELECT mg.movie FROM MovieGenre mg WHERE mg.genre.id IN :genreIds GROUP BY mg.movie HAVING COUNT(DISTINCT mg.genre.id) = :genreCount")
+  Set<Movie> findMoviesByAllGenreIds(@Param("genreIds") Set<Long> genreIds, @Param("genreCount") Long genreCount);
+}
+
