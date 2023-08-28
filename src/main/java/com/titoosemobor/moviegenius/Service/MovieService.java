5e2eea85643b6fd12fe1f031a9ca5c46f2ded219
Tito-Osemobor <titoosemobor@gmail.com>
2023-08-28 10:43:29 -0400

    Create fetchMoviesByGenre method

--- src/main/java/com/titoosemobor/moviegenius/Service/MovieService.java
+++ src/main/java/com/titoosemobor/moviegenius/Service/MovieService.java
@@ -2,18 +2,38 @@ package com.titoosemobor.moviegenius.Service;
 
 import com.titoosemobor.moviegenius.DTO.MovieDTO;
 import com.titoosemobor.moviegenius.DTO.MovieDTOMapper;
+import com.titoosemobor.moviegenius.Entity.Genre;
 import com.titoosemobor.moviegenius.Entity.Movie;
+import com.titoosemobor.moviegenius.Entity.MovieGenre;
+import com.titoosemobor.moviegenius.Repository.GenreRepository;
+import com.titoosemobor.moviegenius.Repository.MovieGenreRepository;
 import com.titoosemobor.moviegenius.Repository.MovieRespository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class MovieService {
   @Autowired
-  MovieRespository movieRespository;
+  private MovieRespository movieRespository;
   @Autowired
-  MovieDTOMapper movieDTOMapper;
+  private GenreRepository genreRepository;
+  @Autowired
+  private MovieGenreRepository movieGenreRepository;
+  @Autowired
+  private MovieDTOMapper movieDTOMapper;
 
+  public Set<MovieDTO> fetchMoviesByGenre(Set<String> genreNames) {
+    Set<Long> genreIds = genreRepository.findAllByNameIn(genreNames)
+      .stream()
+      .map(Genre::getId)
+      .collect(Collectors.toSet());
+    Set<Movie> movies = movieGenreRepository.findMoviesByAllGenreIds(genreIds, (long) genreIds.size());
+    return movies
+      .stream()
+      .map(movieDTOMapper)
+      .collect(Collectors.toSet());
+  }
 }
