20e2284098b4bccf74197bb83442e47fa9d0e2a8
Tito-Osemobor <titoosemobor@gmail.com>
2023-08-30 20:59:27 -0400

    Defined respective json property for fields

--- src/main/java/com/titoosemobor/moviegenius/DTO/UserProfileDTO.java
+++ src/main/java/com/titoosemobor/moviegenius/DTO/UserProfileDTO.java
@@ -1,6 +1,7 @@
 package com.titoosemobor.moviegenius.DTO;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,7 +14,10 @@ import java.sql.Timestamp;
 @AllArgsConstructor
 public class UserProfileDTO {
   private Long id;
+  @JsonProperty("profile_name")
   private String profileName;
+  @JsonProperty("profile_image")
   private String profileImage;
+  @JsonProperty("created_at")
   private Timestamp createdAt;
 }
