86a96adc10e1bf8a9bc9f56ca460156c4d47e9ab
Tito-Osemobor <titoosemobor@gmail.com>
2024-02-05 22:16:45 -0500

    Change import paths

--- movie-genius-frontend/src/App.js
+++ movie-genius-frontend/src/App.js
@@ -1,7 +1,7 @@
 
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Login from "./pages/Login/Login";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
