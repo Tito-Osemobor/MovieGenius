b3c1179c212f40d3c932fa4aa08b040c1b102616
Tito-Osemobor <titoosemobor@gmail.com>
2024-02-06 00:41:42 -0500

    Create reusable component for footer

---
+++ movie-genius-frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className={"items-end justify-center text-center"}>
+      <h2>Made by Tito Osemobor</h2>
+    </footer>
+  )
+}
+
+export default Footer;
