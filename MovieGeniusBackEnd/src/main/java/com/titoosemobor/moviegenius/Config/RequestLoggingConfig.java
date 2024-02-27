0b86650a758a5638e6abc5571687be2e31ebb853
Tito-Osemobor <titoosemobor@gmail.com>
2024-02-27 06:26:48 -0500

    Create config for show api logs

---
+++ MovieGeniusBackEnd/src/main/java/com/titoosemobor/moviegenius/Config/RequestLoggingConfig.java
@@ -0,0 +1,18 @@
+package com.titoosemobor.moviegenius.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingConfig {
+  @Bean
+  public CommonsRequestLoggingFilter requestLoggingFilter() {
+    CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+    loggingFilter.setIncludeClientInfo(true);
+    loggingFilter.setIncludeQueryString(true);
+    loggingFilter.setIncludePayload(true);
+    loggingFilter.setIncludeHeaders(true);
+    return loggingFilter;
+  }
+}
