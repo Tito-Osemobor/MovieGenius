04c0df4095e00eb935c974c934a10b6efd89c9df
Tito-Osemobor <titoosemobor@gmail.com>
2024-02-27 06:27:32 -0500

    Create a repository layer method for findRandomPosters

--- MovieGeniusBackEnd/src/main/java/com/titoosemobor/moviegenius/Repository/MovieRepository.java
+++ MovieGeniusBackEnd/src/main/java/com/titoosemobor/moviegenius/Repository/MovieRepository.java
@@ -2,9 +2,15 @@ package com.titoosemobor.moviegenius.Repository;
 
 import com.titoosemobor.moviegenius.Entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Set;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
   Movie findMovieById(Long id);
+
+  @Query(value = "SELECT poster_path FROM movies ORDER BY random() LIMIT 18", nativeQuery = true)
+  Set<String> findRandomPosters();
 }
