e89b5ce9002fad33bbf0c95e977b7de76fb4c974
Tito-Osemobor <titoosemobor@gmail.com>
2024-04-04 22:21:21 -0400

    Implement exception handling, exists endpoint and dynamic cross-origin url

--- src/main/java/com/titoosemobor/moviegenius/Auth/AuthenticationController.java
+++ src/main/java/com/titoosemobor/moviegenius/Auth/AuthenticationController.java
@@ -3,13 +3,18 @@ package com.titoosemobor.moviegenius.Auth;
 import com.titoosemobor.moviegenius.Exception.UserException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
+@CrossOrigin(origins = "${allowed.origins}")
 @RequiredArgsConstructor
 public class AuthenticationController {
+  @Value("${allowed.origins}")
+  private String allowedOrigins;
+
   @Autowired
   private final AuthenticationService authenticationService;
 
@@ -26,9 +31,18 @@ public class AuthenticationController {
   }
 
   @PostMapping("/login")
-  public ResponseEntity<AuthenticationResponse> register (
+  public ResponseEntity<?> signIn (
     @RequestBody AuthenticationRequest request
   ) {
-    return ResponseEntity.ok(authenticationService.authenticate(request));
+    try {
+      return ResponseEntity.ok(authenticationService.authenticate(request));
+    } catch (UserException.AuthenticationFailedException | UserException.InvalidInformationException ex) {
+      return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+  }
+
+  @GetMapping("/exists")
+  public ResponseEntity<?> exists (@RequestHeader String email) {
+    return ResponseEntity.ok(authenticationService.isEmailAlreadyUsed(email));
   }
 }
